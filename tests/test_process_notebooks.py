e0016efb040b69e7972c863c0f0df81dabc00695
Michael Waskom <mwaskom@nyu.edu>
2021-06-01 11:55:49 -0400

    Add test for check-execution logic

--- tests/test_process_notebooks.py
+++ tests/test_process_notebooks.py
@@ -12,8 +12,8 @@ def test_raises_not_implemented_error(cmd):
     nb = "tutorials/raises_notimplemented_error.ipynb"
     cmdline = cmd + ["--check-only", "--execute", nb]
     res = run(cmdline, capture_output=True)
-    assert nb in res.stdout.decode("utf-8")
     assert not res.returncode
+    assert nb in res.stdout.decode("utf-8")
 
 
 def test_raises_name_error(cmd):
@@ -30,8 +30,37 @@ def test_raises_name_error(cmd):
 def test_executed_out_of_order(cmd):
 
     nb = "tutorials/executed_out_of_order.ipynb"
-    cmdline = cmd + ["--check-only", "--execute", nb]
+    cmdline = cmd + ["--check-only", nb]
     res = run(cmdline, capture_output=True)
     assert res.returncode
     assert nb in res.stderr.decode("utf-8")
     assert "not sequentially executed" in res.stderr.decode("utf-8")
+
+
+def test_executed_partially(cmd):
+
+    nb = "tutorials/executed_partially.ipynb"
+    cmdline = cmd + ["--check-only", "--check-execution", nb]
+    res = run(cmdline, capture_output=True)
+    assert res.returncode
+    assert nb in res.stderr.decode("utf-8")
+    assert "has unexecuted code cell(s)" in res.stderr.decode("utf-8")
+
+
+def test_executed_with_error(cmd):
+
+    nb = "tutorials/executed_with_error.ipynb"
+    cmdline = cmd + ["--check-only", "--check-execution", nb]
+    res = run(cmdline, capture_output=True)
+    assert res.returncode
+    assert nb in res.stderr.decode("utf-8")
+    assert "NameError" in res.stderr.decode("utf-8")
+
+
+def test_executed_successfully(cmd):
+
+    nb = "tutorials/executed_successfully.ipynb"
+    cmdline = cmd + ["--check-only", "--check-execution", nb]
+    res = run(cmdline, capture_output=True)
+    assert not res.returncode
+    assert nb in res.stdout.decode("utf-8")
