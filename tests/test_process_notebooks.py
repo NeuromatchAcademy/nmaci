f0142930b01706457ef081e685a4c5687052dea8
Michael Waskom <mwaskom@nyu.edu>
2021-06-01 11:12:06 -0400

    Add execution flag to notebook tests

--- tests/test_process_notebooks.py
+++ tests/test_process_notebooks.py
@@ -1,19 +1,25 @@
 from subprocess import run
+from pytest import fixture
 
 
-def test_raises_not_implemented_error():
+@fixture
+def cmd():
+    return ["python", "scripts/process_notebooks.py"]
+
+
+def test_raises_not_implemented_error(cmd):
 
     nb = "tutorials/raises_notimplemented_error.ipynb"
-    cmdline = ["python", "scripts/process_notebooks.py", "--check-only", nb]
+    cmdline = cmd + ["--check-only", "--execute", nb]
     res = run(cmdline, capture_output=True)
     assert nb in res.stdout.decode("utf-8")
     assert not res.returncode
 
 
-def test_raises_name_error():
+def test_raises_name_error(cmd):
 
     nb = "tutorials/raises_name_error.ipynb"
-    cmdline = ["python", "scripts/process_notebooks.py", "--check-only", nb]
+    cmdline = cmd + ["--check-only", "--execute", nb]
     res = run(cmdline, capture_output=True)
     assert res.returncode
     assert nb in res.stdout.decode("utf-8")
@@ -21,10 +27,10 @@ def test_raises_name_error():
     assert "NameError" in res.stderr.decode("utf-8")
 
 
-def test_executed_out_of_order():
+def test_executed_out_of_order(cmd):
 
     nb = "tutorials/executed_out_of_order.ipynb"
-    cmdline = ["python", "scripts/process_notebooks.py", "--check-only", nb]
+    cmdline = cmd + ["--check-only", "--execute", nb]
     res = run(cmdline, capture_output=True)
     assert res.returncode
     assert nb in res.stderr.decode("utf-8")
