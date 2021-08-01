6850fde5f5548ca97103f79d107e7854853fbf5a
Spiros Ch <spirosChv@users.noreply.github.com>
2021-08-01 21:28:40 +0300

    Update generate_book_dl.py

--- scripts/generate_book_dl.py
+++ scripts/generate_book_dl.py
@@ -90,7 +90,7 @@ def main():
             if m['category'] == category:
                 # Add and process all notebooks
                 try:
-                    this_section['sections'].append({'file': f"projects/{category}/{m['link']}", 'title': m['title']})
+                    this_section['sections'].append({'file': f"projects/{category}/{m['link']}"})
                     pre_process_notebook(f"projects/{category}/{m['link']}")
                 except:
                     pass
